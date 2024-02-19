f61e110bfc44854e9f27c40a2386fec9e377610f
Kaylan Souza <kaylansouza2@gmai.com>
2024-02-19 17:55:27 -0300

    Começando Projeto E-commerce com NextJS

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Site E-commerce",
+  description: "Criando um site e-commerce com NextJS e entre outras técnologias",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
